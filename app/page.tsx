3244f426583d125aeb0351197c6d877ba046705a
georgf1rst <zhorik06bondarenko@gmail.com>
2024-06-19 17:29:14 +0500

    1

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="w-full h-full flex justify-between items-center">
+    <div className="w-full h-full flex items-center justify-center">
       <Button>Miras pidoras</Button>
     </div>
   );
