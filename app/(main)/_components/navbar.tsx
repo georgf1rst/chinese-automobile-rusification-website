8ff162e16ad73a64933c529ad61827dae5029977
georgf1rst <zhorik06bondarenko@gmail.com>
2024-06-23 20:16:11 +0500

    adaptive

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -1,16 +1,25 @@
 import NavbarRoutes from "@/components/navbar-routes";
 import { Logo } from "./logo";
+import MobileSidebar from "./mobile-sidebar";
 
 const Navbar = () => {
     return ( 
-        <div className="p-4 border-b h-full flex items-center bg-white shadow-sm justify-between">
+        <div className="p-4 border-b h-full flex items-center bg-white shadow-sm w-full justify-between">
 
-            <div className="flex py-28 justify-center md:w-[180px]">
+            <div className="hidden py-28 justify-center md:scale-125 md:w-[180px] xl:scale-150 xl:w-[240px] md:flex">
                 <Logo />
             </div>
 
-            <NavbarRoutes/>
-        
+            <MobileSidebar/>
+            
+            <div className="hidden py-28 justify-center md:hidden sm:flex">
+                <Logo />
+            </div>
+
+            <div className="">
+                <NavbarRoutes/>
+            </div>
+            
         </div>
      );
 }
