21036f4e59a8b758307cbc836810f50f472097dc
georgf1rst <zhorik06bondarenko@gmail.com>
2024-06-23 16:25:25 +0500

    header numbers

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Ghost, PhoneCall } from "lucide-react";
+import { PhoneCall } from "lucide-react";
 import { Button } from "./ui/button";
 import { Popover,
          PopoverContent,
@@ -21,22 +21,53 @@ const NavbarRoutes = () => {
                 </Button>
             </PopoverTrigger>
             <PopoverContent>
-                <div className="grid gap-2">
+                <div className=" grid gap-2">
                     <div className="space-y-2">
                         <p className="text-sm text-muted-foreground">
                             Наши контакты:
                         </p>
                     </div>
-                    <div className="grid gap-3">
-                        <p>
-                            <a href={`tel:${87023923222}`}>+7 702 392 3222</a>
-                        </p>
-                        <p>
-                            <a href={`tel:${87023923222}`}>+7 702 392 3222</a>
-                        </p>
+                    <div className="flex-col md:hidden">
+                        <a href={`tel:${87023923222}`} className="flex gap-2 items-center hover:bg-slate-200 transition pt-1 pb-2 px-2 rounded-md">
+                            <h3 className="text-md text-slate-800">Влад:</h3>
+                            <p>
+                                +7 702 392 3222
+                            </p>
+                        </a>
+                        <a href={`tel:${87058707885}`} className="flex gap-2 items-center hover:bg-slate-200 transition pt-1 pb-2 px-2 rounded-md">
+                            <h3 className="text-md text-slate-800">Макс:</h3>
+                            <p>
+                                +7 705 870 7885
+                            </p>
+                        </a>
+                        <a href={`tel:${87077050852}`} className="flex gap-2 items-center hover:bg-slate-200 transition pt-1 pb-2 px-2 rounded-md">
+                            <h3 className="text-md text-slate-800">Георгий:</h3>
+                            <p>
+                                +7 707 705 0852
+                            </p>
+                        </a>
+                    </div>
+                    <div className="flex-col hidden md:flex">
+                        <div className="flex gap-2 items-center hover:bg-slate-200 transition pt-1 pb-2 px-2 rounded-md">
+                            <h3 className="text-md text-slate-800">Влад:</h3>
+                            <p>
+                                +7 702 392 3222
+                            </p>
+                        </div>
+                        <div className="flex gap-2 items-center hover:bg-slate-200 transition pt-1 pb-2 px-2 rounded-md">
+                            <h3 className="text-md text-slate-800">Макс:</h3>
+                            <p>
+                                +7 705 870 7885
+                            </p>
+                        </div>
+                        <div className="flex gap-2 items-center hover:bg-slate-200 transition pt-1 pb-2 px-2 rounded-md">
+                            <h3 className="text-md text-slate-800">Георгий:</h3>
+                            <p>
+                                +7 707 705 0852
+                            </p>
+                        </div>
                     </div>
                 </div>
-                
             </PopoverContent>
         </Popover>
       </div>
