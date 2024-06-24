0116fb4177fed7c68cbb788ca7e4146e6b48d73b
georgf1rst <zhorik06bondarenko@gmail.com>
2024-06-24 19:59:41 +0500

    location header

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,19 +1,59 @@
 "use client"
 
-import { PhoneCall } from "lucide-react";
+import { MapPin, PhoneCall } from "lucide-react";
 import { Button } from "./ui/button";
 import { Popover,
          PopoverContent,
          PopoverTrigger
  } from "@/components/ui/popover";
+import { useRouter } from "next/navigation";
 
 const NavbarRoutes = () => {
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push(`/address`);
+  }
+
   return (
-    <div className="">
+    <div className="flex gap-2">
       <div>
         {/* TO DO: SEARCH */}
       </div>
       <div>
+        <Popover>
+            <PopoverTrigger>
+                <Button variant="ghost" >
+                    <MapPin className="h-5 w-5"/>
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent >
+                <div className=" grid gap-2">
+                    <div className="space-y-2">
+                        <p className="text-sm text-muted-foreground">
+                            Мы на картах:
+                        </p>
+                    </div>
+                    <div className="flex-col">
+                        <a onClick={onClick} className="flex gap-2 items-center hover:bg-slate-200 transition pt-1 pb-2 px-2 rounded-md cursor-pointer">
+                            <p>
+                                Курмангазы 70Б
+                            </p>
+                        </a>
+                        <a href={`https://yandex.com/maps/-/CDvdjJmz`} className="flex gap-2 items-center hover:bg-slate-200 transition pt-1 pb-2 px-2 rounded-md cursor-pointer">
+                            <p>
+                                Яндекс Карты
+                            </p>
+                        </a>
+                        <a href={`https://maps.app.goo.gl/pWSmmUwydV6qeomk7`} className="flex gap-2 items-center hover:bg-slate-200 transition pt-1 pb-2 px-2 rounded-md cursor-pointer">
+                            <p>
+                                Google Карты
+                            </p>
+                        </a>
+                    </div>
+                </div>
+            </PopoverContent>
+        </Popover>
         <Popover>
             <PopoverTrigger>
                 <Button variant="ghost" >
